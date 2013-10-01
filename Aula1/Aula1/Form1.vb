b2fdc6d02c32b80112b428a66a199214543e6cda
i11857 <i11857@epcjc.net>
2013-10-01 12:26:48 +0100

    A funcionar

--- Aula1/Aula1/Form1.vb
+++ Aula1/Aula1/Form1.vb
@@ -7,4 +7,19 @@
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         End
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim preço, consumo, distancia, res1, res2 As Double
+        preço = TextBox1.Text
+        consumo = TextBox2.Text
+        distancia = TextBox3.Text
+
+        res1 = consumo * distancia / 100
+        res2 = res1 * preço
+
+        TextBox4.Text = res1
+        TextBox5.Text = res2
+
+
+    End Sub
 End Class
