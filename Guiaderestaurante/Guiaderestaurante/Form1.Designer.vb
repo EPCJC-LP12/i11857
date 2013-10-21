f1c3409d8427870cd5a6cdb4b23492aa9246e045
i11857 <i11857@epcjc.net>
2013-10-21 12:52:24 +0100

    Coming

--- Guiaderestaurante/Guiaderestaurante/Form1.Designer.vb
+++ Guiaderestaurante/Guiaderestaurante/Form1.Designer.vb
@@ -23,122 +23,23 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Dim IdLabel As System.Windows.Forms.Label
-        Dim NomeLabel As System.Windows.Forms.Label
-        Dim PaisLabel As System.Windows.Forms.Label
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
-        Me.IdTextBox = New System.Windows.Forms.TextBox()
-        Me.NomeTextBox = New System.Windows.Forms.TextBox()
-        Me.PaisTextBox = New System.Windows.Forms.TextBox()
-        Me.PilotosBindingNavigator = New System.Windows.Forms.BindingNavigator(Me.components)
-        Me.BindingNavigatorAddNewItem = New System.Windows.Forms.ToolStripButton()
-        Me.BindingNavigatorCountItem = New System.Windows.Forms.ToolStripLabel()
-        Me.BindingNavigatorDeleteItem = New System.Windows.Forms.ToolStripButton()
         Me.BindingNavigatorMoveFirstItem = New System.Windows.Forms.ToolStripButton()
         Me.BindingNavigatorMovePreviousItem = New System.Windows.Forms.ToolStripButton()
         Me.BindingNavigatorSeparator = New System.Windows.Forms.ToolStripSeparator()
         Me.BindingNavigatorPositionItem = New System.Windows.Forms.ToolStripTextBox()
+        Me.BindingNavigatorCountItem = New System.Windows.Forms.ToolStripLabel()
         Me.BindingNavigatorSeparator1 = New System.Windows.Forms.ToolStripSeparator()
         Me.BindingNavigatorMoveNextItem = New System.Windows.Forms.ToolStripButton()
         Me.BindingNavigatorMoveLastItem = New System.Windows.Forms.ToolStripButton()
         Me.BindingNavigatorSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.BindingNavigatorAddNewItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorDeleteItem = New System.Windows.Forms.ToolStripButton()
         Me.PilotosBindingNavigatorSaveItem = New System.Windows.Forms.ToolStripButton()
-        IdLabel = New System.Windows.Forms.Label()
-        NomeLabel = New System.Windows.Forms.Label()
-        PaisLabel = New System.Windows.Forms.Label()
+        Me.PilotosBindingNavigator = New System.Windows.Forms.BindingNavigator(Me.components)
         CType(Me.PilotosBindingNavigator, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.PilotosBindingNavigator.SuspendLayout()
         Me.SuspendLayout()
-        '
-        'IdLabel
-        '
-        IdLabel.AutoSize = True
-        IdLabel.Location = New System.Drawing.Point(94, 67)
-        IdLabel.Name = "IdLabel"
-        IdLabel.Size = New System.Drawing.Size(18, 13)
-        IdLabel.TabIndex = 8
-        IdLabel.Text = "id:"
-        '
-        'IdTextBox
-        '
-        Me.IdTextBox.Location = New System.Drawing.Point(136, 64)
-        Me.IdTextBox.Name = "IdTextBox"
-        Me.IdTextBox.Size = New System.Drawing.Size(100, 20)
-        Me.IdTextBox.TabIndex = 9
-        '
-        'NomeLabel
-        '
-        NomeLabel.AutoSize = True
-        NomeLabel.Location = New System.Drawing.Point(94, 93)
-        NomeLabel.Name = "NomeLabel"
-        NomeLabel.Size = New System.Drawing.Size(36, 13)
-        NomeLabel.TabIndex = 10
-        NomeLabel.Text = "nome:"
-        '
-        'NomeTextBox
-        '
-        Me.NomeTextBox.Location = New System.Drawing.Point(136, 90)
-        Me.NomeTextBox.Name = "NomeTextBox"
-        Me.NomeTextBox.Size = New System.Drawing.Size(100, 20)
-        Me.NomeTextBox.TabIndex = 11
-        '
-        'PaisLabel
-        '
-        PaisLabel.AutoSize = True
-        PaisLabel.Location = New System.Drawing.Point(94, 119)
-        PaisLabel.Name = "PaisLabel"
-        PaisLabel.Size = New System.Drawing.Size(29, 13)
-        PaisLabel.TabIndex = 12
-        PaisLabel.Text = "pais:"
-        '
-        'PaisTextBox
-        '
-        Me.PaisTextBox.Location = New System.Drawing.Point(136, 116)
-        Me.PaisTextBox.Name = "PaisTextBox"
-        Me.PaisTextBox.Size = New System.Drawing.Size(100, 20)
-        Me.PaisTextBox.TabIndex = 13
-        '
-        'PilotosBindingNavigator
-        '
-        Me.PilotosBindingNavigator.AddNewItem = Me.BindingNavigatorAddNewItem
-        Me.PilotosBindingNavigator.CountItem = Me.BindingNavigatorCountItem
-        Me.PilotosBindingNavigator.DeleteItem = Me.BindingNavigatorDeleteItem
-        Me.PilotosBindingNavigator.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.BindingNavigatorMoveFirstItem, Me.BindingNavigatorMovePreviousItem, Me.BindingNavigatorSeparator, Me.BindingNavigatorPositionItem, Me.BindingNavigatorCountItem, Me.BindingNavigatorSeparator1, Me.BindingNavigatorMoveNextItem, Me.BindingNavigatorMoveLastItem, Me.BindingNavigatorSeparator2, Me.BindingNavigatorAddNewItem, Me.BindingNavigatorDeleteItem, Me.PilotosBindingNavigatorSaveItem})
-        Me.PilotosBindingNavigator.Location = New System.Drawing.Point(0, 0)
-        Me.PilotosBindingNavigator.MoveFirstItem = Me.BindingNavigatorMoveFirstItem
-        Me.PilotosBindingNavigator.MoveLastItem = Me.BindingNavigatorMoveLastItem
-        Me.PilotosBindingNavigator.MoveNextItem = Me.BindingNavigatorMoveNextItem
-        Me.PilotosBindingNavigator.MovePreviousItem = Me.BindingNavigatorMovePreviousItem
-        Me.PilotosBindingNavigator.Name = "PilotosBindingNavigator"
-        Me.PilotosBindingNavigator.PositionItem = Me.BindingNavigatorPositionItem
-        Me.PilotosBindingNavigator.Size = New System.Drawing.Size(374, 25)
-        Me.PilotosBindingNavigator.TabIndex = 7
-        Me.PilotosBindingNavigator.Text = "BindingNavigator1"
-        '
-        'BindingNavigatorAddNewItem
-        '
-        Me.BindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
-        Me.BindingNavigatorAddNewItem.Image = CType(resources.GetObject("BindingNavigatorAddNewItem.Image"), System.Drawing.Image)
-        Me.BindingNavigatorAddNewItem.Name = "BindingNavigatorAddNewItem"
-        Me.BindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = True
-        Me.BindingNavigatorAddNewItem.Size = New System.Drawing.Size(23, 22)
-        Me.BindingNavigatorAddNewItem.Text = "Add new"
-        '
-        'BindingNavigatorCountItem
-        '
-        Me.BindingNavigatorCountItem.Name = "BindingNavigatorCountItem"
-        Me.BindingNavigatorCountItem.Size = New System.Drawing.Size(35, 22)
-        Me.BindingNavigatorCountItem.Text = "of {0}"
-        Me.BindingNavigatorCountItem.ToolTipText = "Total number of items"
-        '
-        'BindingNavigatorDeleteItem
-        '
-        Me.BindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
-        Me.BindingNavigatorDeleteItem.Image = CType(resources.GetObject("BindingNavigatorDeleteItem.Image"), System.Drawing.Image)
-        Me.BindingNavigatorDeleteItem.Name = "BindingNavigatorDeleteItem"
-        Me.BindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = True
-        Me.BindingNavigatorDeleteItem.Size = New System.Drawing.Size(23, 22)
-        Me.BindingNavigatorDeleteItem.Text = "Delete"
         '
         'BindingNavigatorMoveFirstItem
         '
@@ -172,6 +73,13 @@ Partial Class Form1
         Me.BindingNavigatorPositionItem.Text = "0"
         Me.BindingNavigatorPositionItem.ToolTipText = "Current position"
         '
+        'BindingNavigatorCountItem
+        '
+        Me.BindingNavigatorCountItem.Name = "BindingNavigatorCountItem"
+        Me.BindingNavigatorCountItem.Size = New System.Drawing.Size(35, 22)
+        Me.BindingNavigatorCountItem.Text = "of {0}"
+        Me.BindingNavigatorCountItem.ToolTipText = "Total number of items"
+        '
         'BindingNavigatorSeparator1
         '
         Me.BindingNavigatorSeparator1.Name = "BindingNavigatorSeparator1"
@@ -200,6 +108,24 @@ Partial Class Form1
         Me.BindingNavigatorSeparator2.Name = "BindingNavigatorSeparator2"
         Me.BindingNavigatorSeparator2.Size = New System.Drawing.Size(6, 25)
         '
+        'BindingNavigatorAddNewItem
+        '
+        Me.BindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorAddNewItem.Image = CType(resources.GetObject("BindingNavigatorAddNewItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorAddNewItem.Name = "BindingNavigatorAddNewItem"
+        Me.BindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorAddNewItem.Size = New System.Drawing.Size(23, 22)
+        Me.BindingNavigatorAddNewItem.Text = "Add new"
+        '
+        'BindingNavigatorDeleteItem
+        '
+        Me.BindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorDeleteItem.Image = CType(resources.GetObject("BindingNavigatorDeleteItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorDeleteItem.Name = "BindingNavigatorDeleteItem"
+        Me.BindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorDeleteItem.Size = New System.Drawing.Size(23, 22)
+        Me.BindingNavigatorDeleteItem.Text = "Delete"
+        '
         'PilotosBindingNavigatorSaveItem
         '
         Me.PilotosBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
@@ -208,17 +134,28 @@ Partial Class Form1
         Me.PilotosBindingNavigatorSaveItem.Size = New System.Drawing.Size(23, 22)
         Me.PilotosBindingNavigatorSaveItem.Text = "Save Data"
         '
+        'PilotosBindingNavigator
+        '
+        Me.PilotosBindingNavigator.AddNewItem = Me.BindingNavigatorAddNewItem
+        Me.PilotosBindingNavigator.CountItem = Me.BindingNavigatorCountItem
+        Me.PilotosBindingNavigator.DeleteItem = Me.BindingNavigatorDeleteItem
+        Me.PilotosBindingNavigator.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.BindingNavigatorMoveFirstItem, Me.BindingNavigatorMovePreviousItem, Me.BindingNavigatorSeparator, Me.BindingNavigatorPositionItem, Me.BindingNavigatorCountItem, Me.BindingNavigatorSeparator1, Me.BindingNavigatorMoveNextItem, Me.BindingNavigatorMoveLastItem, Me.BindingNavigatorSeparator2, Me.BindingNavigatorAddNewItem, Me.BindingNavigatorDeleteItem, Me.PilotosBindingNavigatorSaveItem})
+        Me.PilotosBindingNavigator.Location = New System.Drawing.Point(0, 0)
+        Me.PilotosBindingNavigator.MoveFirstItem = Me.BindingNavigatorMoveFirstItem
+        Me.PilotosBindingNavigator.MoveLastItem = Me.BindingNavigatorMoveLastItem
+        Me.PilotosBindingNavigator.MoveNextItem = Me.BindingNavigatorMoveNextItem
+        Me.PilotosBindingNavigator.MovePreviousItem = Me.BindingNavigatorMovePreviousItem
+        Me.PilotosBindingNavigator.Name = "PilotosBindingNavigator"
+        Me.PilotosBindingNavigator.PositionItem = Me.BindingNavigatorPositionItem
+        Me.PilotosBindingNavigator.Size = New System.Drawing.Size(374, 25)
+        Me.PilotosBindingNavigator.TabIndex = 7
+        Me.PilotosBindingNavigator.Text = "BindingNavigator1"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(374, 261)
-        Me.Controls.Add(IdLabel)
-        Me.Controls.Add(Me.IdTextBox)
-        Me.Controls.Add(NomeLabel)
-        Me.Controls.Add(Me.NomeTextBox)
-        Me.Controls.Add(PaisLabel)
-        Me.Controls.Add(Me.PaisTextBox)
         Me.Controls.Add(Me.PilotosBindingNavigator)
         Me.Name = "Form1"
         CType(Me.PilotosBindingNavigator, System.ComponentModel.ISupportInitialize).EndInit()
@@ -228,21 +165,18 @@ Partial Class Form1
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents IdTextBox As System.Windows.Forms.TextBox
-    Friend WithEvents NomeTextBox As System.Windows.Forms.TextBox
-    Friend WithEvents PaisTextBox As System.Windows.Forms.TextBox
-    Friend WithEvents PilotosBindingNavigator As System.Windows.Forms.BindingNavigator
-    Friend WithEvents BindingNavigatorAddNewItem As System.Windows.Forms.ToolStripButton
-    Friend WithEvents BindingNavigatorCountItem As System.Windows.Forms.ToolStripLabel
-    Friend WithEvents BindingNavigatorDeleteItem As System.Windows.Forms.ToolStripButton
     Friend WithEvents BindingNavigatorMoveFirstItem As System.Windows.Forms.ToolStripButton
     Friend WithEvents BindingNavigatorMovePreviousItem As System.Windows.Forms.ToolStripButton
     Friend WithEvents BindingNavigatorSeparator As System.Windows.Forms.ToolStripSeparator
     Friend WithEvents BindingNavigatorPositionItem As System.Windows.Forms.ToolStripTextBox
+    Friend WithEvents BindingNavigatorCountItem As System.Windows.Forms.ToolStripLabel
     Friend WithEvents BindingNavigatorSeparator1 As System.Windows.Forms.ToolStripSeparator
     Friend WithEvents BindingNavigatorMoveNextItem As System.Windows.Forms.ToolStripButton
     Friend WithEvents BindingNavigatorMoveLastItem As System.Windows.Forms.ToolStripButton
     Friend WithEvents BindingNavigatorSeparator2 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents BindingNavigatorAddNewItem As System.Windows.Forms.ToolStripButton
+    Friend WithEvents BindingNavigatorDeleteItem As System.Windows.Forms.ToolStripButton
     Friend WithEvents PilotosBindingNavigatorSaveItem As System.Windows.Forms.ToolStripButton
+    Friend WithEvents PilotosBindingNavigator As System.Windows.Forms.BindingNavigator
 
 End Class
